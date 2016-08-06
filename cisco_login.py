deed9cd5c3703013acca2742ef9704e3efd74a2b
t3lpiz <rtelpiz@mail.ru>
2016-08-06 23:42:24 +0300

    test cisco

---
+++ cisco_login.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get('http://86.106.213.22:8080/home')
+try:
+    username = driver.find_element_by_name('username')
+    username.send_keys('username')
+    password = driver.find_element_by_name('password')
+    password.send_keys('parola')
+    buton_ok = driver.find_element_by_xpath('//input[@type="submit"]')
+    buton_ok.click()
+except Exception:
+    pass
+driver.quit()
